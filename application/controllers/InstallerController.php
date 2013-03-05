778078e6fb6b0fc08f727bd1b7b9ed74a646edcf
Menno Dekker <menno.dekker@erasmusmc.nl>
2013-03-05 12:33:20 +0100

    Added /assets directory to the install checks

--- application/controllers/InstallerController.php
+++ application/controllers/InstallerController.php
@@ -840,6 +840,9 @@ class InstallerController extends CController {
         if (!check_DirectoryWriteable(Yii::app()->getConfig('uploaddir').'/', $aData, 'uploaddir', 'uerror',true) )
             $bProceed = false;
         
+        if (!check_DirectoryWriteable(Yii::app()->getConfig('rootdir').'/assets', $data, 'assetsdir', 'aerror',true) )
+            $bProceed = false;
+        
         // Session writable check
         $session = Yii::app()->session; /* @var $session CHttpSession */
         $sessionWritable = ($session->get('saveCheck', null)==='save');
